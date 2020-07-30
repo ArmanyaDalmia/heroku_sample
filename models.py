6ef0962d16e12b9a00af24a38a6087ac24f95cc9
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-29 22:28:14 -0400

    Update models.py

--- models.py
+++ models.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_filename = os.environ['DATABASE_URL']
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = os.environ['DATABASE_URL']
 
 
 db = SQLAlchemy()
@@ -19,8 +17,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
-
 
 '''
 Person
